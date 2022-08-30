fe6f6eddd0207c34e4957c246d5f3e8dfe7c6a5d
AJAYI OLAYIDE <ebena107@gmail.com>
2022-08-30 13:22:19 +0400

    project boilerplate
    initial commit

---
+++ data/script.js
@@ -0,0 +1,5 @@
+/* ESP32 WEBSOCKET jAVASCRIPT FILE
+/   -----------------------
+/ BY: OLAYIDE AJAYI
+/--------------------------
+*/
---
+++ esp32_Websocket.ino
@@ -0,0 +1,16 @@
+#include <DHT.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
+#include <ArduinoJson.h>
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
